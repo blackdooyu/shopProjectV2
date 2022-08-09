fb37d9b69dfdc8fc04517cdccaf0fd7204553946
blackdooyu <84924161+blackdooyu@users.noreply.github.com>
2022-08-09 23:31:51 +0900

    feat : API 응답 객체 개발

---
+++ src/main/java/refactor/shopproject/api/common/ApiResult.java
@@ -0,0 +1,14 @@
+package refactor.shopproject.api.common;
+
+import org.springframework.http.HttpStatus;
+
+public class ApiResult<T> {
+
+    private T result;
+    private HttpStatus status;
+
+    public ApiResult(T result) {
+        this.result = result;
+        this.status = HttpStatus.OK;
+    }
+}
